4854ba519aff2ae9505eee97e9e5acc5a1154a18
iWaann20 <ichwanfirdaus04@gmail.com>
2025-03-20 10:51:32 +0700

    hapus login with

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -74,29 +74,6 @@
                                                 <button class="btn btn-primary w-100 waves-effect waves-light" type="submit" style="background-color: #023669 !important; border-color: #023669 !important;">Sign In</button>
                                             </div>
                                         </form>
-                                        <div class="mt-4 pt-2 text-center">
-                                            <div class="signin-other-title">
-                                                <h5 class="font-size-14 mb-3 text-muted fw-medium">- Sign in with -</h5>
-                                            </div>
-        
-                                            <ul class="list-inline mb-0">
-                                                <li class="list-inline-item">
-                                                    <a href="javascript:void()" class="social-list-item bg-primary text-white border-primary" style="background-color: #023669 !important; border-color: #023669 !important;">
-                                                        <i class="mdi mdi-facebook"></i>
-                                                    </a>
-                                                </li>
-                                                <li class="list-inline-item">
-                                                    <a href="javascript:void()" class="social-list-item bg-info text-white border-info">
-                                                        <i class="mdi mdi-twitter"></i>
-                                                    </a>
-                                                </li>
-                                                <li class="list-inline-item">
-                                                    <a href="javascript:void()" class="social-list-item bg-danger text-white border-danger">
-                                                        <i class="mdi mdi-google"></i>
-                                                    </a>
-                                                </li>
-                                            </ul>
-                                        </div>
                                         <div class="mt-3 text-center">
                                             <p class="text-muted mb-0">Don't have an account? <a href="/signup" class="text-primary fw-semibold" style="color: #023669 !important;">Sign up now</a></p>
                                         </div>
